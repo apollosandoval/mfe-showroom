64eb5d3c30ab1b340df78ac900664111834d9db3
oscar.sandovalreynoso <oscar.sandovalreynoso@carvana.com>
2022-04-29 13:42:39 -0700

    add menu component

---
+++ packages/menu/lib/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = props => {
+  return (
+    <nav>
+      This is a menu
+    </nav>
+  )
+};
+
+export default Menu;
---
+++ packages/menu/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  entry: './lib/index.js',
+  mode: 'development',
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path:  path.resolve(__dirname, 'dist/'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public/'),
+      publicPath: 'http://localhost:3000/',
+    },
+    port: 3000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env']
+            }
+          },
+          {
+            loader: '@linaria/webpack-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new ModuleFederationPlugin({
+      name: 'showroom_components',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './Menu': './lib/index.js'
+      },
+      shared: require('./package.json').dependencies
+    })
+  ]
+};
